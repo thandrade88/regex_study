307840b51c85ce7884b28fce61a478f850a1e8d9
thales <thandrade88gmail.com>
2018-01-22 11:04:30 +0000

    1- Password validation

---
+++ 1_validade_password.js
@@ -0,0 +1,14 @@
+function validate(password) {
+    var patt = new RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{6,}");
+    return patt.test(password);
+}
+
+
+console.log(validate('djI38D55'), 'djI38D55 - Expected true');
+console.log(validate('a2.d412'), 'a2.d412 - Expected false');
+console.log(validate('JHD5FJ53'), 'JHD5FJ53 - Expected false');
+console.log(validate('!fdjn345'), '!fdjn345 - Expected false');
+console.log(validate('jfkdfj3j'), 'jfkdfj3j - Expected false');
+console.log(validate('123'), '123 - Expected false');
+console.log(validate('abc'), 'abc - Expected false');
+console.log(validate('Password123'), 'Password123 - Expected true');
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # regex_study
+
+This document shows a couple of exercices to improve my knowledge about regex.
+The solved problems below are extracted from CodeWars.com.
+
+1) You need to write regex that will validate a password to make sure it meets the following criteria:
+   
+   At least six characters long
+   contains a lowercase letter
+   contains an uppercase letter
+   contains a number
+   Valid passwords will only be alphanumeric characters.
